2d9a800742272bd1c53a3302b816f1fff74ed849
yetone <i@yetone.net>
2014-11-11 16:38:21 +0800

    fix

--- static/js/a.js
+++ static/js/a.js
@@ -41,7 +41,7 @@ $(function() {
     $nextPage.hide();
     loading();
     var token = getCookie('token');
-    if (!token || token === 'undefined' && window.location.hash.indexOf('#/register') > -1) {
+    if (!token || token === 'undefined' && window.location.hash.indexOf('#/register') < 0) {
       window.location.hash = '#/login/';
     }
   });
